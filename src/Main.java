30c745e1c517bcecc3dd6798515f51c5e93dd4a0
Fabian Felix Kraus <fabianfelix.kraus@exxeta.com>
2022-08-04 13:10:01 +0200

    Added Name

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
         // some other change
-        System.out.println("Hello World");
+        System.out.println("Hello World Fabian");
     }
 }
